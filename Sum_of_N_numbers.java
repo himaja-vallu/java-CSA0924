9e09df08c3356bff85a9da78b12f08f251c144f9
himaja-vallu <113410100+himaja-vallu@users.noreply.github.com>
2024-04-06 13:32:26 +0530

    Create Sum_of_N_numbers.java

---
+++ Sum_of_N_numbers.java
@@ -0,0 +1,17 @@
+//Sum of N numbers
+
+import java.util.*;
+class MyProgram {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int n,i;
+        int sum=0;
+        System.out.print("Enter the number:");
+        n=s.nextInt();
+        for(i=0;i<=n;i++)
+        {
+            sum=sum+i;
+        }
+        System.out.printf("The sum of given %d numbers is = %d",n,sum);
+    }
+}
